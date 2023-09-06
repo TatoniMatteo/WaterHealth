dc7b18713028060c5dcf0cebc38a965aae3d720e
Matteo Tatoni <83955605+TatoniMatteo@users.noreply.github.com>
2023-09-06 17:57:43 +0200

    update

--- app/src/main/java/com/tatonimatteo/waterhealth/entity/Record.java
+++ app/src/main/java/com/tatonimatteo/waterhealth/entity/Record.java
@@ -3,6 +3,7 @@ package com.tatonimatteo.waterhealth.entity;
 import androidx.annotation.NonNull;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Record {
     private final long id;
@@ -43,5 +44,30 @@ public class Record {
     @Override
     public String toString() {
         return getDateTime() + " - " + (stationId * 1000 + sensorId) + ": " + value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Record record = (Record) o;
+
+        if (id != record.id) return false;
+        if (stationId != record.stationId) return false;
+        if (sensorId != record.sensorId) return false;
+        if (!Objects.equals(dateTime, record.dateTime))
+            return false;
+        return Objects.equals(value, record.value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (dateTime != null ? dateTime.hashCode() : 0);
+        result = 31 * result + (int) (stationId ^ (stationId >>> 32));
+        result = 31 * result + (int) (sensorId ^ (sensorId >>> 32));
+        result = 31 * result + (value != null ? value.hashCode() : 0);
+        return result;
     }
 }
